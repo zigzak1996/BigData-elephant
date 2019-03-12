f43d7fea8198400edb986311acee0d685160e277
mishabuzov <mikhaijl@gmail.com>
2019-03-12 20:38:16 +0300

    put vocabulary creating inside mapper, regular expression added, cleaned the code

--- src/ru/innopolis/university/homework/Indexer.java
+++ src/ru/innopolis/university/homework/Indexer.java
@@ -1,4 +1,5 @@
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -10,125 +11,136 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Indexer {
 
-    public static Map vocab = new HashMap<String, Integer>();
+    public static class TFMapper extends Mapper<Object, Text, IntWritable, IntWritable> {
 
-    public static final String PATH_TO_VOC = "/home/misha/IdeaProjects/WordCount/vocabulary/vocabulary";
+        private static final Map<String, Integer> vocab = new HashMap<>();
 
-    public static void createVocabulary() {
-        BufferedReader bf;
-        try {
-            System.out.println("START");
-            bf = new BufferedReader(new FileReader(PATH_TO_VOC));
-            String line = bf.readLine();
+        private final IntWritable docIds = new IntWritable();
 
-            while (line != null) {
-                StringTokenizer itr = new StringTokenizer(line);
-                String key = itr.nextToken();
-                Integer value = Integer.parseInt(itr.nextToken());
-                vocab.putIfAbsent(key, value);
+        private final IntWritable wordIds = new IntWritable();
 
-                line = bf.readLine();
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            super.setup(context);
+
+            Configuration conf = context.getConfiguration();
+
+            String raw_path_to_vocabulary = conf.get("path_to_vocabulary");
+
+            Path path_to_vocabulary = new Path(raw_path_to_vocabulary);
+            FileSystem fileSystem = FileSystem.get(conf);
+
+            try (BufferedReader bf = new BufferedReader(new InputStreamReader(fileSystem.open(path_to_vocabulary)))) {
+
+                String line = bf.readLine();
+
+                while (line != null) {
+                    StringTokenizer itr = new StringTokenizer(line);
+
+                    String word = itr.nextToken();
+
+                    Integer word_id = Integer.parseInt(itr.nextToken());
+
+                    vocab.putIfAbsent(word, word_id);
+
+                    line = bf.readLine();
+                }
+
+            } catch (IOException ex) {
+                System.out.println("Error reading file!");
+                ex.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+
         }
 
-    }
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-
-    public static class TokenizerMapper
-            extends Mapper<Object, Text, IntWritable, Text> {
-
-        private Text word = new Text();
-
-        public void map(Object key, Text value, Context context
-        ) throws IOException, InterruptedException {
             JSONObject json = new JSONObject(value.toString());
 
-            StringTokenizer itr = new StringTokenizer(json.getString("text"));
+            String text = json.getString("text").toLowerCase().replaceAll("[^a-z0-9\\s]", " ");
+
+            StringTokenizer itr = new StringTokenizer(text);
+
+            int docId = json.getInt("id");
+
+            docIds.set(docId);
 
             while (itr.hasMoreTokens()) {
 
-                String word = itr.nextToken().toLowerCase();
+                String word = itr.nextToken();
 
-                StringBuilder parseWord = new StringBuilder();
+                if (vocab.containsKey(word)) {
 
-                for (int i = 0; i < word.length(); i++) {
+                    wordIds.set(vocab.get(word));
 
-                    if ((word.charAt(i) >= 'a' && word.charAt(i) <= 'z') || word.charAt(i) == '-') {
-
-                        parseWord.append(word.charAt(i));
-
-                    }
-
+                    context.write(docIds, wordIds);
                 }
-                if (vocab.containsKey(parseWord.toString())) {
-                    int k = json.getInt("id"); //id doc-a;
-                    int v = (int) vocab.get(parseWord.toString()); // id slova v doke;
-                    this.word.set(Integer.toString(v));
-                    context.write(new IntWritable(k), this.word);
-                }
-
             }
         }
     }
 
-    public static class IntSumReducer
-            extends Reducer<IntWritable, Text, IntWritable, Text> {
+    public static class TFReducer extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
 
-        public void reduce(IntWritable key, Iterable<Text> values, Context context) {
-            Map<String, Integer> count = new HashMap<>();
-            System.out.print(key.get());
-            for (Text word_id_in_doc : values) {
+        private static final String OUTPUT_FORMAT = "%s\t%d";
 
-                count.merge(word_id_in_doc.toString(), 1, Integer::sum);
+        @Override
+        public void reduce(IntWritable docId, Iterable<IntWritable> wordIds, Context context)
+                throws IOException, InterruptedException {
 
+            final Map<String, Integer> wordsTFcount = new HashMap<>();
+
+            for (IntWritable wordId : wordIds) {  // put here wordId and count wordId met in spec. corpus (TF)
+                wordsTFcount.merge(String.valueOf(wordId), 1, Integer::sum);
             }
-            count.forEach((k, v) -> {
-                Text text = new Text();
-                text.set(String.format("%s\t%s", k, Integer.toString(v)));
-                try {
-                    context.write(key, text);
-                } catch (IOException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
 
+            Text wordIdAndTF = new Text();
+
+            for (String wordId : wordsTFcount.keySet()) {
+                wordIdAndTF.set(String.format(OUTPUT_FORMAT, wordId, wordsTFcount.get(wordId)));
+                context.write(docId, wordIdAndTF);
+                wordIdAndTF.clear();
+            }
 
         }
     }
 
-    public static class Combiner extends Reducer<IntWritable, Text, IntWritable, Text> {
-        public void reduce(IntWritable key, Iterable<Text> value, Context context) throws IOException, InterruptedException {
-            for (Text v : value)
-                context.write(key, v);
+    public static class Combiner extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+
+        @Override
+        public void reduce(IntWritable docId, Iterable<IntWritable> wordIds, Context context)
+                throws IOException, InterruptedException {
+
+            for (IntWritable wordId : wordIds) {
+                context.write(docId, wordId);
+            }
+
         }
 
     }
 
     public static void main(String[] args) throws Exception {
-        createVocabulary();
-
         Configuration conf = new Configuration();
+        conf.set("path_to_vocabulary", args[0]);
         Job job = Job.getInstance(conf, "Indexer");
         job.setJarByClass(Indexer.class);
-        job.setMapperClass(TokenizerMapper.class);
+        job.setMapperClass(TFMapper.class);
         job.setCombinerClass(Combiner.class);
-        job.setReducerClass(IntSumReducer.class);
+        job.setReducerClass(TFReducer.class);
         job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
         job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
